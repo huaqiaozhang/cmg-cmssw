bac44a02d6c546b2203dc4a81ccb9853c51d80b2
Colin <colin.bernet@cern.ch>
2014-04-16 09:07:48 +0200

    jet_gen macro: style passed to plot constructor

--- CMGTools/PFPaper/python/macros/jet_gen.py
+++ CMGTools/PFPaper/python/macros/jet_gen.py
@@ -12,13 +12,6 @@ for style in styles:
     style.markerSize = 0.8
     style.markerStyle = 4
 
-
-class Profile( object ):
-    def __init__(self, h2d):
-        self.mean = h2d.ProfileX()
-        self.rms = rmsX(h2d) 
-        
-
     
 fun_respt = TF1('fun_respt',
                 'sqrt( ([0]/x)**2 + '
@@ -28,7 +21,8 @@ fun_respt = TF1('fun_respt',
 
 class Residual(object):
     
-    def __init__(self, name, xtitle, binsx, binsy, canx=800, cany=800, logx=False):
+    def __init__(self, name, xtitle, binsx, binsy, canx=800, cany=800,
+                 logx=False, style=sRed):
         self.name = name
         self.logx = logx
         nx = len(binsx)-1
@@ -40,11 +34,11 @@ class Residual(object):
         self.canvas = None
         self.canx=canx
         self.cany=cany
+        self.style=style
                          
     def fill(self, tree, var, cut):
         tree.Draw('{var}>>+{hname}'.format(var=var, hname=self.name),
                   cut, 'goff')
-
 
     def fit_slice(self, ibin):
         slice = self.h2d.ProjectionY( '{name}_bin_{bin}'.format(name=self.name,
@@ -52,7 +46,6 @@ class Residual(object):
         slice.Fit('gaus')
         gPad.Update()
             
-
     def fit(self):
         self.h2d.FitSlicesY()
         self.mean = gDirectory.Get(self.name + '_1')
@@ -63,6 +56,8 @@ class Residual(object):
         self.armean = self.h2d.ProfileX()
         self.rms = rmsX( self.h2d )
         self.rms.Divide(self.mean)
+        self.style.formatHisto(self.mean)
+        self.style.formatHisto(self.sigma)
 
     def fit_resolution(self, function_name):
         results = self.sigma.Fit(function_name, 'S', 'goff')
@@ -72,30 +67,33 @@ class Residual(object):
         print self.name, function_name, chi2/ndf
         return results 
 
-    def draw(self, style):
+    def draw(self):
         self.canvas = TCanvas(self.name, self.name, self.canx, self.cany )
         self.canvas.Divide(2,2)
-        if self.logx:
-            for ipad in range(4):
-                self.canvas.cd(ipad+1)
-                gPad.SetLogx()
-                gPad.SetGridx()
-                gPad.SetGridy()
+        
+        self.canvas.GetPad(2).SetGridx()
+        self.canvas.GetPad(2).SetGridy()            
+
+        self.canvas.GetPad(3).SetLogx()
+        self.canvas.GetPad(3).SetGridx()
+        self.canvas.GetPad(3).SetGridy()            
+        
         self.canvas.cd(1)
         self.h2d.Draw('col')
         self.canvas.cd(2)
         draw(self.mean, self.h2d.GetXaxis().GetTitle(), 'response', 
-             0.5, 1.1, style, gPad)
-        self.armean.SetLineColor(style.lineColor)
+             0.5, 1.1, self.style, gPad)
+        self.armean.SetLineColor(self.style.lineColor)
         self.armean.Draw('same')
         self.canvas.cd(3)
         draw(self.sigma, self.h2d.GetXaxis().GetTitle(), 'resolution',
-             0, 0.45, style, gPad) 
-        self.rms.SetLineColor(style.lineColor)
+             0, 0.45, self.style, gPad) 
+        self.rms.SetLineColor(self.style.lineColor)
         self.rms.Draw('same')
         self.canvas.cd(4)
         gPad.SetLogx(0)
         self.fit_slice(1)
+
 
 
 def rmsX( h2d, graphics=False ):
@@ -119,6 +117,7 @@ def rmsX( h2d, graphics=False ):
     return hist
 
 
+
 def draw(hist, xtitle, ytitle, ymin, ymax, style, pad=None, options=''):
     if pad is None:
         pad = TCanvas(hist.GetName(), hist.GetName(),
@@ -135,10 +134,12 @@ def draw(hist, xtitle, ytitle, ymin, ymax, style, pad=None, options=''):
     return pad
 
 
-def build_plot( name, file_pattern, binsx, binsy, ptcut):
+
+
+def build_plot( name, file_pattern, binsx, binsy, ptcut, style):
     chain = Chain(None, file_pattern)
     plot =  Residual(name, 'p_{T} (GeV)',
-                     binsx, binsy, logx=True)
+                     binsx, binsy, logx=True, style=style)
     plot.fill(
         chain,
         'jet1_pt / jet1_genJet_pt : jet1_genJet_pt',
@@ -155,12 +156,13 @@ def build_plot( name, file_pattern, binsx, binsy, ptcut):
     # results = plot.fit_resolution('fun_respt')
     return plot
 
+  
 
 if __name__ == '__main__':
 
     import sys
 
-    binsx_low = np.linspace( 10, 300, 30)
+    binsx_low = np.linspace( 20, 300, 29)
     binsx_high = np.linspace( 350, 1000, 14)
     binsx = np.concatenate( [binsx_low, binsx_high] )    
     nbinsy = 100
@@ -169,58 +171,12 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     pf_file_pattern = args[0]
     pf_plot = build_plot( 'pf_barrel', pf_file_pattern, binsx, binsy,
-                          ptcut=1)
-    pf_plot.draw(sRed)
+                          ptcut=5, style=sRed)
+    pf_plot.draw()
     if len(args)==2:
         calo_file_pattern = args[1]
         calo_plot = build_plot( 'calo_barrel', calo_file_pattern, binsx, binsy,
-                                ptcut=1)
-        calo_plot.draw(sBlue)
-
-    # chain.SetAlias('jet1_dr2', '(jet1_eta - jet1_genJet_eta)**2 + (jet1_phi-jet1_genJet_phi)**2')
-    # chain.SetAlias('jet2_dr2', '(jet2_eta - jet2_genJet_eta)**2 + (jet2_phi-jet2_genJet_phi)**2')
-    
+                                ptcut=3, style=sBlue)
+        calo_plot.draw()
 
 
-
-##     plot_vs_pt_endcaps =  Residual('plot_vs_pt_endcaps', 'p_{T} (GeV)',
-##                                    nbinsx, 0, 200, nbinsy, 0, 3)
-##     plot_vs_pt_endcaps.fill( chain,
-##                  'jet1_pt / jet1_genJet_pt : jet1_genJet_pt',
-##                  'jet1_genJet_pt>0 && abs(jet1_eta)>1.6 && abs(jet1_eta)<2.9')
-##     plot_vs_pt_endcaps.fill( chain,
-##                  'jet2_pt / jet2_genJet_pt : jet2_genJet_pt',
-##                  'jet2_genJet_pt>0 && abs(jet2_eta)>1.6 && abs(jet2_eta)<2.9')
-##     plot_vs_pt_endcaps.fit()
-##     plot_vs_pt_endcaps.sigma.Fit('fun_respt')
-##     plot_vs_pt_endcaps.draw()
-
-
-##     plot_vs_pt_hf =  Residual('plot_vs_pt_hf', 'p_{T} (GeV)',
-##                               nbinsx, 0, 200, nbinsy, 0, 3)
-##     plot_vs_pt_hf.fill( chain,
-##                  'jet1_pt / jet1_genJet_pt : jet1_genJet_pt',
-##                  'jet1_genJet_pt>0 && abs(jet1_eta)>3.1 && abs(jet1_eta)<4.8')
-##     plot_vs_pt_hf.fill( chain,
-##                  'jet2_pt / jet2_genJet_pt : jet2_genJet_pt',
-##                  'jet2_genJet_pt>0 && abs(jet2_eta)>3.1 && abs(jet2_eta)<4.8')
-##     plot_vs_pt_hf.fit()
-##     plot_vs_pt_hf.sigma.Fit('fun_respt')
-##     plot_vs_pt_hf.draw()
-
-
-
-
-##     plot_vs_eta =  Residual('plot_vs_eta', '#eta',
-##                             nbinsx, -5, 5, nbinsy, 0, 3)
-##     plot_vs_eta.fill( chain,
-##                       'jet1_pt / jet1_genJet_eta : jet1_genJet_eta',
-##                       'jet1_pt>20')
-##     plot_vs_eta.fill( chain,
-##                       'jet2_pt / jet2_genJet_pt : jet2_genJet_eta',
-##                       'jet2_pt>20')
-##     plot_vs_eta.fit()
-##     plot_vs_eta.draw()
-
-
-    
